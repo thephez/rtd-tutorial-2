a84d3b27d665da4ba054b6bca40501c4183cafc1
thephez <thephez@users.noreply.github.com>
2022-12-01 13:11:36 -0500

    style: update theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -39,8 +39,9 @@ templates_path = ['_templates']
 
 # -- Options for HTML output
 
-html_theme = 'sphinx_rtd_theme'
+# html_theme = 'sphinx_rtd_theme'
 # html_theme = 'sphinx_immaterial'
+html_theme = "pydata_sphinx_theme"
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
